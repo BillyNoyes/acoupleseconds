e693ec4eab8d4c35fd51ba82a4f6340f4e0830c0
William (Billy) Noyes <wbillypn@gmail.com>
2020-07-17 16:36:52 +0100

    Updates

---
+++ precache-manifest.ee7a2ed0a292ac66c5f894ecab745b4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af8ccc5abbb75d6a0b8c3e840076c9f5",
+    "url": "/acoupleseconds/index.html"
+  },
+  {
+    "revision": "bd37ba1ad85b0ee47e75",
+    "url": "/acoupleseconds/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f0108459ab19a72e81da",
+    "url": "/acoupleseconds/static/js/2.976b8d33.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/acoupleseconds/static/js/2.976b8d33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd37ba1ad85b0ee47e75",
+    "url": "/acoupleseconds/static/js/main.e39b59ea.chunk.js"
+  },
+  {
+    "revision": "622f1f1c6d731b4ab711",
+    "url": "/acoupleseconds/static/js/runtime-main.fc751038.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/acoupleseconds/static/media/logo.5d5d9eef.svg"
+  }
+]);
